80e844dffcdf96552d519dccde21e560b4ef270b
ep <ep16018@sti.chubu.ac.jp>
2020-01-30 14:15:09 +0900

    gccss

--- kadai2-ep17113.c
+++ kadai2-ep17113.c
@@ -11,8 +11,7 @@ int main(){
   }
 
   for(i=0;i<n;i++){
-    scanf("%d",&num);
-    //printf("aa\n");
+    scanf("%d",&num);    
     h=num%13;
     while(a[h]!=(-1)){
       num2=num+1;
@@ -22,7 +21,7 @@ int main(){
   }
 
   for(i=0;i<13;i++){
-    printf("%d",a[i]);
+    printf("%d ",a[i]);
   }  
   printf("\n");
 }
