0677b7ee8630b719e009f48ae50df93e95c9cc97
ep <ep16018@sti.chubu.ac.jp>
2020-01-30 15:50:58 +0900

    gccss

--- kadai1-EP17080.c
+++ kadai1-EP17080.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 int main(void){
   int x,i,n,y[32];
-  scanf("%ld\n",&n);
+  scanf("%d\n",&n);
   for(i=0;i<31;i++){
     y[i]=0;
   }
   for(i=0;i<n;i++){
-    scanf("%ld",&x);
+    scanf("%d",&x);
     y[x]+=1;
   }
   for(i=0;i<31;i++){
-    printf("%ld",y[i]);
+    printf("%d",y[i]);
   }
   return 0;
 }
