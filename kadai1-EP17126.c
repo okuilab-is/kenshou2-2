3d4b26c7ad5229b7a1f85ef9a1473926d0064ef0
ep <ep16018@sti.chubu.ac.jp>
2020-01-30 15:40:45 +0900

    gccss

---
+++ kadai1-EP17126.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+	int n,i,x[32]=0,m;
+	scanf("%d",&n);
+//	for(i=0;i<32;i++){
+//		x[i]=0;
+//	}
+	for(i=0;i<n;i++){
+		scanf("%d",&m);
+		x[m]++;
+	}
+	for(i=0;i<32;i++){
+		for(1;x[i]>0;x[i]--){
+			printf("%d",i);
+		}
+	}
+	prntf("\n");
+}
