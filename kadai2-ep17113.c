77098436d00d0f61bd8a2e37ca8572ee8afcc907
ep <ep16018@sti.chubu.ac.jp>
2020-01-30 14:10:02 +0900

    gccss

---
+++ kadai2-ep17113.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+
+int main(){
+  int i,num,h,num2;
+
+  scanf("%d",&n);
+ 
+  int a[13];
+  for(i=0;i<13;i++){
+    a[13]=-1;
+  }
+  for(i=0;i<n;i++){
+    scanf("%d",&num);
+    h=num%13;
+    while(a[h]!=(-1)){
+      num2=num+1;
+      h=num2%13;
+    }  
+    a[h]=num;
+  }
+  for(i=0;i<13;i++){
+    printf("%d",a[i]);
+  }  
+
+}
