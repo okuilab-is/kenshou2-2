ba6ed6fafec35e64963ea79fbec969c610cd4c3c
ep <ep16018@sti.chubu.ac.jp>
2020-01-30 13:57:07 +0900

    gccss

--- kadai1-1-ep17112.c
+++ kadai1-1-ep17112.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 
 int main(){
-  int n,i,x,p[32];
-	for(i=0;i<32;i++) p[i]=0;
+	int n,i,x,p[32]={};
+//	for(i=0;i<32;i++) p[i]=0;
   scanf("%d",&n);
 	for(i=0;i<n;i++){
 	  scanf("%d",&x);
