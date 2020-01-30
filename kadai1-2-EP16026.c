326fc663ac13d235f6b9baadd060fca55775ea46
ep <ep16018@sti.chubu.ac.jp>
2020-01-30 16:41:28 +0900

    gccss

--- kadai1-2-EP16026.c
+++ kadai1-2-EP16026.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 //EP16026 Ogura.K
 int main(void){
-  int n,m,i,bag[32]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+  int n,m,i,j,bag[32]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
   scanf("%d\n",&n);
   for(i=0;i<n;++i){
     scanf("%d",&m);
     bag[m]=bag[m]+1;
   }
   for(i=0;i<32;++i){
-    printf("%d",bag[i]);
+    k=bag[i];
+    for(j=0;j<k;++j){
+      printf("%d",i);
   }
   printf("\n");
   return 0;
