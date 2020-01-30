8653092fa308442c77a57ef3420cec492e0bc854
ep <ep16018@sti.chubu.ac.jp>
2020-01-30 14:16:48 +0900

    gccss

--- kadai2-1-ep17046.c
+++ kadai2-1-ep17046.c
@@ -2,11 +2,15 @@
 
 int main(void){
  int h[13],n,m=13,a,i;
- for(i=0;i<m,i++) h[i]=-1;
+ for(i=0;i<m,i++){
+  h[i]=-1;
+ }
  scanf("%d",&n);
  for(i=0;i<n;i++){
   scanf("%d",&a);
   h[a%m]=a;
  }
- for(i=0;i<m;i++) printf("%d ",h[i]);
+ for(i=0;i<m;i++)
+  printf("%d ",h[i]);
+ }
 }
