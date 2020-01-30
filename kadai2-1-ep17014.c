24a83130d1bde5c108c95eeeff02dad3467a713a
ep <ep16018@sti.chubu.ac.jp>
2020-01-30 16:20:39 +0900

    gccss

---
+++ kadai2-1-ep17014.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void){
+  int i,max,num,a,ans[32];
+  scanf("%d",&num);
+  for(i=0;i<32;i++)ans[i]=-1;
+  for(i=0;i<num;i++){
+    scanf("%d",&a);
+    ans[a%13]=a;
+  }
+
+}
