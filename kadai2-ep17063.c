ed8a45992997cd2ae4b31d22b73e62a4b7a7756b
ep <ep16018@sti.chubu.ac.jp>
2020-01-30 15:51:47 +0900

    gccss

--- kadai2-ep17063.c
+++ kadai2-ep17063.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
-
+#define H 13
 int main(){
-  int num,i,k,n,h[8];
-  for(i=0;i<8;i++){
+  int num,i,k,n,h[H];
+  for(i=0;i<H;i++){
     h[i] = -1;
   }
   scanf("%d",&n);
 
   for(i=0;i<n;i++){
     scanf("%d",&num);
-    k = num % 8;
+    k = num % H;
     while(h[k]!=-1){
       k++;
     }
     h[k] = num;
   }
 
-  for(i=0;i<8;i++){
+  for(i=0;i<H;i++){
     printf("%d \n",h[i]);  
   }
   return 0;
